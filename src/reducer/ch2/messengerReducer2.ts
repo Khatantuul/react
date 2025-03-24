c825cb76736b6750205fd08baeec9a7a4bae4fcf
Khatantuul Batbold <batbold.k@northeastern.edu>
2025-03-24 19:35:30 -0400

    Challenge 2 of reducer

---
+++ src/reducer/ch2/messengerReducer2.ts
@@ -0,0 +1,27 @@
+export const initialState = {
+    selectedId: 0,
+    message: ''
+}
+
+type ActionType = 'CHANGE SELECTION' | 'EDIT MESSAGE' | 'SEND MESSAGE';
+export interface ReducerAction{
+    type: ActionType,
+    contactId?: number, 
+    message?: string
+}
+
+export const reducer = (state: typeof initialState, action: ReducerAction) =>{
+    switch (action.type){
+        case 'CHANGE SELECTION':
+            return {...state, selectedId: (action.contactId ?? state.selectedId), message: ''}
+        case 'EDIT MESSAGE':
+        case 'SEND MESSAGE':
+            return {...state, message: action.message ?? ''}
+        // case 'SEND MESSAGE':
+        //     return {...state, message: action.message ?? ''}
+    }
+}
+
+//when our selection id is 0 it evaluates to false, thus the first item in data is not getting selected
+//Instead of using ? : (ternary operator with a falsy check), use the nullish coalescing operator (??), 
+// which only checks for null or undefined
