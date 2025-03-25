dff5de6f64f1ee6e5fcbccd52d7fb3c2d6cc6772
Khatantuul Batbold <batbold.k@northeastern.edu>
2025-03-25 11:57:52 -0400

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,30 @@
-import { useState, useRef, useEffect } from 'react'
-import Child from './Child';
-import Button from './Button';
-import ButtonWithStyle from './ButtonWithStyle';
-
-type User = {
-  name: string
-}
+// import Game from "./tictactoe/Game";
+import CommentList from "./commentStructure/Comments";
+import EmployeeList from "./employees/Employees";
+import Form from "./form/Form";
+import FormWithReducer from "./form/FormWithReducer";
+import Counter from "./reducer/basic/Counter";
+import Messenger from "./reducer/ch1/ContactList";
+import Messenger2 from "./reducer/ch2/ContactList2";
+import Messenger3 from "./reducer/ch3/ContactList3";
+import TaskApp from "./reducer/TaskApp";
+import {
+  LocationContext,
+  LocationContextProvider,
+} from "./testInterview/LocationContextProvider";
+import MainInterview from "./testInterview/MainInterview";
+import TravelPlan from "./travelPlan/TravelPlan";
 
 function App() {
-  const [count, setCount] = useState(2)
-  //correctly infers the primitive types so no need for useState<number>
-
-  // const [user, setUser] = useState(null);
-  //but here because we initialize null, it cannot know it will be object
-  const [user, setUser] = useState<User | null>(null);
-  const name = user?.name;
-  //be careful when accessing
-
-  const products = [
-    { title: 'Cabbage', isFruit: false, id: 1 },
-    { title: 'Garlic', isFruit: false, id: 2 },
-    { title: 'Apple', isFruit: true, id: 3 },
-  ] as const;
-
-  const buttonRef = useRef(null);
-
-  useEffect(() => {
-    console.log("Button element:", buttonRef.current); //no need for Child to accept it with forwardRef from React.19 
-  }, []); 
-
   return (
-    <>
-      <h1>Hi</h1>
-      {
-        products.map((item)=>{
-          console.log('item is ', item.title);
-          
-          return <p key={item.id} style={{color: item.isFruit ? 'green': 'orange'}}>{item.title} at {item.id}</p>
-        })
-      }
-      <div>
-        <Child count={20} name='Khatna'>
-          <Button style={{
-            fontSize: '32px'
-          }}/>
-          <ButtonWithStyle borderRadius={{
-            topLeft: 10,
-            topRight: 10,
-            bottomRight: 10,
-            bottomLeft: 10
-
-          }}
-          ref={buttonRef}
-          />
-          </Child>
-      </div>
-    </>
-  )
+    // <LocationContextProvider>
+    //   <MainInterview />
+    // </LocationContextProvider>
+    // <TaskApp />
+    // <Messenger3 />
+    // <Form />
+    <FormWithReducer />
+  );
 }
 
-export default App
+export default App;
